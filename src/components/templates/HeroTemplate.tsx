22c92bb997f7bceaf89ac4b79329de061044f0fe
Shrihari <sriharikatti88@gmail.com>
2025-09-14 23:03:07 +0530

    Initial commit: ChanciFy prediction platform setup

---
+++ src/components/templates/HeroTemplate.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function HeroTemplate() {
+  return (
+    <div>
+      {/* app Icon   */}
+      <div>
+        <div>{/* chart icon  */}</div>
+      </div>
+
+      {/* Main heading  */}
+      <h1>Predict the Future</h1>
+
+      {/* Description */}
+      <p>
+        Join thousands making predictions on sports, politics, and entertainment
+      </p>
+
+      {/* Features list */}
+      <div>
+        <div>⚡ Live Results</div>
+        <div>👥 Community</div>
+        <div>📊 Real Odds</div>
+      </div>
+    </div>
+  );
+}
